39133d6bebbb2403a18175eb423bdc4a9e2909c7
Taliesin Sisson <taliesins@yahoo.com>
2018-03-16 14:19:20 +0000

    Add tests for RS256,RS384,RS512,PS256,PS384,PS512

--- middlewares/jwt/jwt_validator_test.go
+++ middlewares/jwt/jwt_validator_test.go
@@ -65,6 +65,300 @@ func signHeaderWithCertificate(req *http.Request, certificate *traefiktls.Certif
 	return nil
 }
 
+func TestWithPS256Success(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	certPath := path.Join(path.Dir(filename), "signing/rsa")
+
+	certificate := &traefiktls.Certificate{
+		CertFile: traefiktls.FileOrContent(fmt.Sprintf("%s.crt", certPath)),
+		KeyFile:  traefiktls.FileOrContent(fmt.Sprintf("%s.key", certPath)),
+	}
+
+	if  !certificate.CertFile.IsPath() {
+		panic(fmt.Errorf("CertFile path is invalid: %s", string(certificate.CertFile)))
+	}
+
+	if  !certificate.KeyFile.IsPath() {
+		panic(fmt.Errorf("KeyFile path is invalid: %s", string(certificate.KeyFile)))
+	}
+
+	certContent, err := certificate.CertFile.Read()
+	if err != nil{
+		panic(err)
+	}
+
+	jwtMiddleware, err := NewJwtValidator(&types.Jwt{
+		Cert: string(certContent),
+	}, &tracing.Tracing{})
+	assert.NoError(t, err, "there should be no error")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "traefik")
+	})
+	n := negroni.New(jwtMiddleware.Handler)
+	n.UseHandler(handler)
+	ts := httptest.NewServer(n)
+	defer ts.Close()
+
+	client := &http.Client{}
+	req := testhelpers.MustNewRequest(http.MethodGet, ts.URL, nil)
+	signHeaderWithCertificate(req, certificate, jwt.SigningMethodPS256)
+
+	res, err := client.Do(req)
+
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "they should be equal")
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, "traefik\n", string(body), "they should be equal")
+}
+
+func TestWithPS384Success(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	certPath := path.Join(path.Dir(filename), "signing/rsa")
+
+	certificate := &traefiktls.Certificate{
+		CertFile: traefiktls.FileOrContent(fmt.Sprintf("%s.crt", certPath)),
+		KeyFile:  traefiktls.FileOrContent(fmt.Sprintf("%s.key", certPath)),
+	}
+
+	if  !certificate.CertFile.IsPath() {
+		panic(fmt.Errorf("CertFile path is invalid: %s", string(certificate.CertFile)))
+	}
+
+	if  !certificate.KeyFile.IsPath() {
+		panic(fmt.Errorf("KeyFile path is invalid: %s", string(certificate.KeyFile)))
+	}
+
+	certContent, err := certificate.CertFile.Read()
+	if err != nil{
+		panic(err)
+	}
+
+	jwtMiddleware, err := NewJwtValidator(&types.Jwt{
+		Cert: string(certContent),
+	}, &tracing.Tracing{})
+	assert.NoError(t, err, "there should be no error")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "traefik")
+	})
+	n := negroni.New(jwtMiddleware.Handler)
+	n.UseHandler(handler)
+	ts := httptest.NewServer(n)
+	defer ts.Close()
+
+	client := &http.Client{}
+	req := testhelpers.MustNewRequest(http.MethodGet, ts.URL, nil)
+	signHeaderWithCertificate(req, certificate, jwt.SigningMethodPS384)
+
+	res, err := client.Do(req)
+
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "they should be equal")
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, "traefik\n", string(body), "they should be equal")
+}
+
+func TestWithPS512Success(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	certPath := path.Join(path.Dir(filename), "signing/rsa")
+
+	certificate := &traefiktls.Certificate{
+		CertFile: traefiktls.FileOrContent(fmt.Sprintf("%s.crt", certPath)),
+		KeyFile:  traefiktls.FileOrContent(fmt.Sprintf("%s.key", certPath)),
+	}
+
+	if  !certificate.CertFile.IsPath() {
+		panic(fmt.Errorf("CertFile path is invalid: %s", string(certificate.CertFile)))
+	}
+
+	if  !certificate.KeyFile.IsPath() {
+		panic(fmt.Errorf("KeyFile path is invalid: %s", string(certificate.KeyFile)))
+	}
+
+	certContent, err := certificate.CertFile.Read()
+	if err != nil{
+		panic(err)
+	}
+
+	jwtMiddleware, err := NewJwtValidator(&types.Jwt{
+		Cert: string(certContent),
+	}, &tracing.Tracing{})
+	assert.NoError(t, err, "there should be no error")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "traefik")
+	})
+	n := negroni.New(jwtMiddleware.Handler)
+	n.UseHandler(handler)
+	ts := httptest.NewServer(n)
+	defer ts.Close()
+
+	client := &http.Client{}
+	req := testhelpers.MustNewRequest(http.MethodGet, ts.URL, nil)
+	signHeaderWithCertificate(req, certificate, jwt.SigningMethodPS512)
+
+	res, err := client.Do(req)
+
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "they should be equal")
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, "traefik\n", string(body), "they should be equal")
+}
+
+func TestWithRS256Success(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	certPath := path.Join(path.Dir(filename), "signing/rsa")
+
+	certificate := &traefiktls.Certificate{
+		CertFile: traefiktls.FileOrContent(fmt.Sprintf("%s.crt", certPath)),
+		KeyFile:  traefiktls.FileOrContent(fmt.Sprintf("%s.key", certPath)),
+	}
+
+	if  !certificate.CertFile.IsPath() {
+		panic(fmt.Errorf("CertFile path is invalid: %s", string(certificate.CertFile)))
+	}
+
+	if  !certificate.KeyFile.IsPath() {
+		panic(fmt.Errorf("KeyFile path is invalid: %s", string(certificate.KeyFile)))
+	}
+
+	certContent, err := certificate.CertFile.Read()
+	if err != nil{
+		panic(err)
+	}
+
+	jwtMiddleware, err := NewJwtValidator(&types.Jwt{
+		Cert: string(certContent),
+	}, &tracing.Tracing{})
+	assert.NoError(t, err, "there should be no error")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "traefik")
+	})
+	n := negroni.New(jwtMiddleware.Handler)
+	n.UseHandler(handler)
+	ts := httptest.NewServer(n)
+	defer ts.Close()
+
+	client := &http.Client{}
+	req := testhelpers.MustNewRequest(http.MethodGet, ts.URL, nil)
+	signHeaderWithCertificate(req, certificate, jwt.SigningMethodRS256)
+
+	res, err := client.Do(req)
+
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "they should be equal")
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, "traefik\n", string(body), "they should be equal")
+}
+
+func TestWithRS384Success(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	certPath := path.Join(path.Dir(filename), "signing/rsa")
+
+	certificate := &traefiktls.Certificate{
+		CertFile: traefiktls.FileOrContent(fmt.Sprintf("%s.crt", certPath)),
+		KeyFile:  traefiktls.FileOrContent(fmt.Sprintf("%s.key", certPath)),
+	}
+
+	if  !certificate.CertFile.IsPath() {
+		panic(fmt.Errorf("CertFile path is invalid: %s", string(certificate.CertFile)))
+	}
+
+	if  !certificate.KeyFile.IsPath() {
+		panic(fmt.Errorf("KeyFile path is invalid: %s", string(certificate.KeyFile)))
+	}
+
+	certContent, err := certificate.CertFile.Read()
+	if err != nil{
+		panic(err)
+	}
+
+	jwtMiddleware, err := NewJwtValidator(&types.Jwt{
+		Cert: string(certContent),
+	}, &tracing.Tracing{})
+	assert.NoError(t, err, "there should be no error")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "traefik")
+	})
+	n := negroni.New(jwtMiddleware.Handler)
+	n.UseHandler(handler)
+	ts := httptest.NewServer(n)
+	defer ts.Close()
+
+	client := &http.Client{}
+	req := testhelpers.MustNewRequest(http.MethodGet, ts.URL, nil)
+	signHeaderWithCertificate(req, certificate, jwt.SigningMethodRS384)
+
+	res, err := client.Do(req)
+
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "they should be equal")
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, "traefik\n", string(body), "they should be equal")
+}
+
+func TestWithRS512Success(t *testing.T) {
+	_, filename, _, _ := runtime.Caller(0)
+	certPath := path.Join(path.Dir(filename), "signing/rsa")
+
+	certificate := &traefiktls.Certificate{
+		CertFile: traefiktls.FileOrContent(fmt.Sprintf("%s.crt", certPath)),
+		KeyFile:  traefiktls.FileOrContent(fmt.Sprintf("%s.key", certPath)),
+	}
+
+	if  !certificate.CertFile.IsPath() {
+		panic(fmt.Errorf("CertFile path is invalid: %s", string(certificate.CertFile)))
+	}
+
+	if  !certificate.KeyFile.IsPath() {
+		panic(fmt.Errorf("KeyFile path is invalid: %s", string(certificate.KeyFile)))
+	}
+
+	certContent, err := certificate.CertFile.Read()
+	if err != nil{
+		panic(err)
+	}
+
+	jwtMiddleware, err := NewJwtValidator(&types.Jwt{
+		Cert: string(certContent),
+	}, &tracing.Tracing{})
+	assert.NoError(t, err, "there should be no error")
+
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "traefik")
+	})
+	n := negroni.New(jwtMiddleware.Handler)
+	n.UseHandler(handler)
+	ts := httptest.NewServer(n)
+	defer ts.Close()
+
+	client := &http.Client{}
+	req := testhelpers.MustNewRequest(http.MethodGet, ts.URL, nil)
+	signHeaderWithCertificate(req, certificate, jwt.SigningMethodRS512)
+
+	res, err := client.Do(req)
+
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, http.StatusOK, res.StatusCode, "they should be equal")
+
+	body, err := ioutil.ReadAll(res.Body)
+	assert.NoError(t, err, "there should be no error")
+	assert.Equal(t, "traefik\n", string(body), "they should be equal")
+}
+
 func TestWithUnsignedRsaPublicKeySuccess(t *testing.T) {
 	_, filename, _, _ := runtime.Caller(0)
 	certPath := path.Join(path.Dir(filename), "signing/rsa")
